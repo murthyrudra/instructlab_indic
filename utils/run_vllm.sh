b0caed4fbf1754b276dff3b0beb1e4de9025279a
Rudra Murthy <rudramurthy.iisc@gmail.com>
2024-09-23 02:52:16 -0400

    Added english and prompt variations

---
+++ utils/run_vllm.sh
@@ -0,0 +1,25 @@
+set -o errexit
+
+# huggingface model id or path
+MODEL_ID=meta-llama/Meta-Llama-3.1-70B-Instruct
+# MODEL_ID=mistralai/Mixtral-8x7B-Instruct-v0.1
+# number of GPUs needed
+NUM_GPUS=2
+set -o nounset
+
+# get a random valid port
+PORT=$(python -c "import socket;sock = socket.socket();sock.bind(('', 0));print(sock.getsockname()[1])")
+
+# print the endpoint information to the log
+echo "starting"
+echo "http://${HOSTNAME}.pok.ibm.com:${PORT}/v1"
+
+python -u -m vllm.entrypoints.openai.api_server \
+    --distributed-executor-backend mp \
+    --disable-custom-all-reduce \
+    --host 0.0.0.0 \
+    --port $PORT \
+    --model $MODEL_ID \
+    --tensor-parallel-size $NUM_GPUS \
+    --load-format safetensors \
+    --gpu-memory-utilization 0.9 --max-model-len 8196
